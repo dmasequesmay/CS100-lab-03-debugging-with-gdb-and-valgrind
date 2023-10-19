c7b956138030cd10a52e9e04ad0af5ee31d2637e
dmasequesmay <dominique.masequesmay@gmail.com>
2023-10-18 18:47:15 -0700

    Bug found and fixed

--- debug.cpp
+++ debug.cpp
@@ -4,8 +4,8 @@
 using namespace std; 
 
 int main() { 
-    //char input[50]; 
-    char* input = NULL;
+    char input[50]; 
+    //char* input = NULL;
     int i = 42; 
     cin.getline(input, 50); 
     for (i = strlen(input); i >= 0; i--) {  
