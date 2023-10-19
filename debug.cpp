378a842ebcd79adca42551faaed27c97c2eb06ef
dmasequesmay <dominique.masequesmay@gmail.com>
2023-10-18 18:43:30 -0700

    Add error to main

--- debug.cpp
+++ debug.cpp
@@ -4,7 +4,8 @@
 using namespace std; 
 
 int main() { 
-    char input[50]; 
+    //char input[50]; 
+    char* input = NULL;
     int i = 42; 
     cin.getline(input, 50); 
     for (i = strlen(input); i >= 0; i--) {  
