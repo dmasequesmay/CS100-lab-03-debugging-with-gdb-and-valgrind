ed3dd3f7433cdb1428dca370d340fe78a5a028d4
William <WilliamMTriDo@gmail.com>
2023-10-25 16:43:03 -0700

    Remove example3 from .gitignore

--- lineage.cpp
+++ lineage.cpp
@@ -9,8 +9,7 @@ int main() {
     theList.addPerson("Jim", "Bob", "Sally");
     theList.addPerson("Frank", "Jim", "Mary");
     theList.addPerson("Leonard", "Jim", "Mary");
-    theList.addPerson("Kim", "Leonard", "Sarah");
-
+    theList.addPerson("Kim", "Leonard", "Sarah")
     theList.printLineage("Jim");
     theList.printLineage("Kim");
     theList.printLineage("Betty");
--- person.cpp
+++ person.cpp
@@ -74,9 +74,9 @@ char* Person::compute_relation(int level){
 /* non-member function which doubles the size of t
  * NOTE: t's type will be a pointer to an array of pointers
  */
-void expand(Person ***t, int *MAX){
-  Person **temp = new Person*[2 * *MAX];
-  memcpy(temp, *t, *MAX * sizeof(**t));
-  *MAX *= 2;
+void expand(Person ***t, int &MAX){
+  Person **temp = new Person*[2 * MAX];
+  memcpy(temp, *t, MAX * sizeof(t));
+  MAX *= 2;
   *t = temp;
 }
