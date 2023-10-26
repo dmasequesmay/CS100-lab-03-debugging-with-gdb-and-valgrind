fc0322cb2aaf2c86aa1db7b620a704477f36fbfa
William <WilliamMTriDo@gmail.com>
2023-10-26 13:45:28 -0700

    Update files

--- lineage.cpp
+++ lineage.cpp
@@ -9,13 +9,13 @@ int main() {
     theList.addPerson("Jim", "Bob", "Sally");
     theList.addPerson("Frank", "Jim", "Mary");
     theList.addPerson("Leonard", "Jim", "Mary");
-    theList.addPerson("Kim", "Leonard", "Sarah")
+    theList.addPerson("Kim", "Leonard", "Sarah");
     theList.printLineage("Jim");
     theList.printLineage("Kim");
     theList.printLineage("Betty");
 
     return 0;
-} 
+}
 
 /* Output (your revised solution should give the same output)
 Ancestors of Jim
